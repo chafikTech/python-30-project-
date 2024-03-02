119113be7180294214ca22081808c482baaffc42
Moahamed chafik <mohmed45chafik@gmail.com>
2024-03-02 11:02:07 +0100

    create public API with flask

---
+++ publicAPI/publicAPI.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+from random import choice, randint, random
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    phrases: list[str] = [
+        "Welcome to this page",
+        "You are looking good today!",
+        "The weather is great!",
+    ]
+    return {"phrase": choice(phrases), "date": datetime.now()}
+
+
+@app.route("/api/random")
+def random():
+    number_input = request.args.get("number", type=int, default=100)
+    text_input = request.args.get("text", type=str, default="default_text")
+
+    if isinstance(number_input, int):
+        return {
+            "input": number_input,
+            "random": randint(0, number_input),
+            "text": text_input,
+            "date": datetime.now()
+        }
+    else:
+        return {"Error": "Please only enter numbers."}
+
+
+if __name__ == "__main__":
+    app.run()
